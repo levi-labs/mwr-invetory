b5bcc3ed8db2b0261b7049e9d2192b9cd16cdb3d
rp.quraisy@gmail.com <rp.quraisy@gmail.com>
2024-08-01 20:28:08 +0700

    fix:title

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ config('app.name') }}</title>
+    <title>{{ $title ?? 'Sistem Informasi Moving Weight Average' }}</title>
     <link rel="stylesheet" href="assets/css/bootstrap.css">
 
     <link rel="shortcut icon" href="{{ asset('/logo_tpp.png') }}" type="image/x-icon">
