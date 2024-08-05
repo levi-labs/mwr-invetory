bdbbfd7d9289c92ad736c62db1d40cd3fff99e92
rp.quraisy@gmail.com <rp.quraisy@gmail.com>
2024-08-05 20:42:02 +0700

    refactor:input kode in form supplier

--- resources/views/pages/supplier/edit.blade.php
+++ resources/views/pages/supplier/edit.blade.php
@@ -32,7 +32,7 @@
                                         <div class="col-12">
                                             <div class="form-group">
                                                 <label for="first-name-vertical">Kode Supplier</label>
-                                                <input type="text" id="first-name-vertical" class="form-control" readonly
+                                                <input type="text" id="first-name-vertical" class="form-control"
                                                     name="kode" value="{{ $supplier->kode }}">
                                                 @error('kode')
                                                     <div class="text-danger">{{ $message }}</div>
